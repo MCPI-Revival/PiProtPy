cb9a8a36a1aa2bab52de4963a700abbb03ae86a6
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-11 23:42:05 +0200

    Update protocol.py

--- piprotpy/protocol.py
+++ piprotpy/protocol.py
@@ -9,3 +9,18 @@ login_status = {
     "id": 0x83,
     "status": None
 }
+
+ready = {
+    "id": 0x84,
+    "status": None
+}
+
+message = {
+    "id": 0x85,
+    "message": None
+}
+
+set_time = {
+    "id": 0x86,
+    "time": None
+}
